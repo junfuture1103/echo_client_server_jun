5580428731795e884eaf5681d7975126ed26081d
junfuture1103 <junhak1103@naver.com>
2021-06-12 19:00:44 +0900

    Replace server logic & Add client

---
+++ client.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+using namespace std;
+
+void usage(char *argv){
+    cout << "Usage : " << argv << " [ip] [port]" << endl;
+    cout << "Example) " << argv << " 127.0.0.1 1234" << endl;
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        usage(argv[0]);
+        return -1;
+    }
+
+    struct sockaddr_in addr_server = {};
+
+    addr_server.sin_family = AF_INET; 
+    addr_server.sin_addr.s_addr = inet_addr(argv[1]); 
+    addr_server.sin_port = htons(atoi(argv[2]));
+
+    char buf[256];
+
+    int sock_client = socket(AF_INET, SOCK_STREAM, 0); // 클라이언트 소켓 생성
+    if(sock_client == -1){
+        printf("socket error");
+        close(sock_client);
+        exit(1);
+    }
+
+    if(connect(sock_client, (struct sockaddr*) &addr_server, sizeof(addr_server)) == -1){ // 연결 요청
+        printf("connect error");
+        close(sock_client);
+        exit(1);
+    }
+   
+    while(1){
+        cin >> buf; // 버퍼에 문자열 입력
+        
+        if(strlen(buf)>255) {
+            break;
+        }
+
+        if(write(sock_client, buf, strlen(buf)) == -1){ //server로 보내기
+            printf("write error");
+            break;
+        
+        }
+        
+    }
+    close(sock_client); // 연결 종료
+    return 0;
+}
+
--- server.cpp
+++ server.cpp
@@ -20,10 +20,14 @@ int main(int argc, char* argv[])
         usage(argv[0]);
         return -1;
     }
-
+    char buf[256];
     struct sockaddr_in addr_server = {};
     struct sockaddr_in addr_client = {};
     socklen_t addr_client_len = sizeof(addr_client);
+    
+    addr_server.sin_family = AF_INET;
+    addr_server.sin_port = htons(atoi(argv[1]));
+    addr_server.sin_addr.s_addr = htonl(INADDR_ANY);
 
     int sock_server = socket(AF_INET, SOCK_STREAM, 0); // 소켓 생성
     if(sock_server == -1){
@@ -32,13 +36,13 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    if(bind(sock_server, (sockaddr*)&addr_server, sizeof(addr_server))==-1){
+    if(bind(sock_server, (sockaddr*)&addr_server, sizeof(addr_server))==-1){ //bind
         printf("bind error\n");
         close(sock_server);
         exit(1);
     }
 
-    if(listen(sock_server, BACKLOG)==-1){
+    if(listen(sock_server, BACKLOG)==-1){ //기다리기
         printf("listen error\n");
         close(sock_server);
         exit(1);
@@ -49,10 +53,19 @@ int main(int argc, char* argv[])
         printf("accept error\n");
         close(sock_server);
         exit(1);
+    }else{
+        printf("accept success.. Hello server to client\n");
     }
 
-    char msg_to_client[] = "Hello this is jun server";
-    write(sock_client, msg_to_client, sizeof(msg_to_client));
+    while(1){
+        memset(buf, 0, 256); // 버퍼 초기화
+        if(read(sock_client, buf, sizeof(buf)-1) == -1){
+            printf("read error");
+            break;
+        }; 
+        
+        printf("%s\n", buf); //클라한테 받은내용 출력
+    }
 
     close(sock_client);
     close(sock_server);
